e05c4afd964c3272655f1b3eaaaeda5ec65d1d00
Murilo Vicente da Silva <murilo.silva@programmers.com.br>
2020-02-08 15:27:31 -0300

    Initial commit

---
+++ SlaveApplication/Startup.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+using LibraryApi.Utils.Swagger;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Microsoft.OpenApi.Models;
+
+namespace SlaveApplication
+{
+    public class Startup
+    {
+        public IConfiguration Configuration { get; }
+        public SwaggerUtils swaggerUtils { get; set; }
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+            swaggerUtils = new SwaggerUtils() { Title = "SlaveApplication", Scope = "slaveApplication" };
+        }        
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            swaggerUtils.ConfigureSwagger(services);
+            services.AddMvc()
+                .SetCompatibilityVersion(CompatibilityVersion.Version_2_2)
+                .AddApplicationPart(Assembly.Load(new AssemblyName("LibraryApi")));
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+            swaggerUtils.ConfigureSwaggerUI(app);
+
+            app.UseHttpsRedirection();
+            app.UseMvc();
+        }        
+    }
+}
